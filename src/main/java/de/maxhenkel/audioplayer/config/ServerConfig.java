22a6760874919c1f07ba6ad2acdea05205e00939
Max Henkel <max@maxhenkel.de>
2023-04-12 13:27:15 +0200

    Add config descriptions

--- src/main/java/de/maxhenkel/audioplayer/config/ServerConfig.java
+++ src/main/java/de/maxhenkel/audioplayer/config/ServerConfig.java
@@ -19,18 +19,90 @@ public class ServerConfig {
     public final ConfigEntry<Integer> cacheSize;
 
     public ServerConfig(ConfigBuilder builder) {
-        filebinUrl = builder.stringEntry("filebin_url", "https://filebin.net/");
-        maxUploadSize = builder.integerEntry("max_upload_size", 1000 * 1000 * 20, 1, Integer.MAX_VALUE);
-        uploadPermissionLevel = builder.integerEntry("upload_permission_level", 0, 0, Integer.MAX_VALUE);
-        applyToItemPermissionLevel = builder.integerEntry("apply_to_item_permission_level", 0, 0, Integer.MAX_VALUE);
-        goatHornCooldown = builder.integerEntry("goat_horn_cooldown", 140, 1, Short.MAX_VALUE);
-        musicDiscRange = builder.doubleEntry("music_disc_range", 65D, 1D, Integer.MAX_VALUE);
-        goatHornRange = builder.doubleEntry("goat_horn_range", 256D, 1D, Integer.MAX_VALUE);
-        allowWavUpload = builder.booleanEntry("allow_wav_upload", true);
-        allowMp3Upload = builder.booleanEntry("allow_mp3_upload", true);
-        maxMusicDiscDuration = builder.integerEntry("max_music_disc_duration", 60 * 5, 1, Integer.MAX_VALUE);
-        maxGoatHornDuration = builder.integerEntry("max_goat_horn_duration", 20, 1, Integer.MAX_VALUE);
-        cacheSize = builder.integerEntry("cache_size", 16, 0, Integer.MAX_VALUE);
+        filebinUrl = builder.stringEntry(
+                "filebin_url",
+                "https://filebin.net/",
+                "The URL of the Filebin service that the mod should use"
+        );
+        maxUploadSize = builder.integerEntry(
+                "max_upload_size",
+                1000 * 1000 * 20,
+                1,
+                Integer.MAX_VALUE,
+                "The maximum allowed size of an uploaded file in bytes"
+        );
+        uploadPermissionLevel = builder.integerEntry(
+                "upload_permission_level",
+                0,
+                0,
+                Integer.MAX_VALUE,
+                "The permission level required to upload a file"
+        );
+        applyToItemPermissionLevel = builder.integerEntry(
+                "apply_to_item_permission_level",
+                0,
+                0,
+                Integer.MAX_VALUE,
+                "The permission level required to apply custom audio to an item"
+        );
+        goatHornCooldown = builder.integerEntry(
+                "goat_horn_cooldown",
+                140,
+                1,
+                Short.MAX_VALUE,
+                "The cooldown of goat horns with custom audio in ticks"
+        );
+        musicDiscRange = builder.doubleEntry(
+                "music_disc_range",
+                65D,
+                1D,
+                Integer.MAX_VALUE,
+                "The range of music discs with custom audio in blocks"
+        );
+        goatHornRange = builder.doubleEntry(
+                "goat_horn_range",
+                256D,
+                1D,
+                Integer.MAX_VALUE,
+                "The range of goat horns with custom audio in blocks"
+        );
+        allowWavUpload = builder.booleanEntry(
+                "allow_wav_upload",
+                true,
+                "Whether users should be able to upload .wav files",
+                "Note that .wav files are not compressed and can be very large",
+                "Playing .wav files may result in more RAM usage"
+        );
+        allowMp3Upload = builder.booleanEntry(
+                "allow_mp3_upload",
+                true,
+                "Whether users should be able to upload .mp3 files",
+                "Note that .mp3 files require Simple Voice Chats mp3 decoder",
+                "Playing .mp3 files can be slightly more CPU intensive"
+        );
+        maxMusicDiscDuration = builder.integerEntry(
+                "max_music_disc_duration",
+                60 * 5,
+                1,
+                Integer.MAX_VALUE,
+                "The maximum allowed duration of a custom music disc in seconds"
+        );
+        maxGoatHornDuration = builder.integerEntry(
+                "max_goat_horn_duration",
+                20,
+                1,
+                Integer.MAX_VALUE,
+                "The maximum allowed duration of a custom goat horn in seconds"
+        );
+        cacheSize = builder.integerEntry(
+                "cache_size",
+                16,
+                0,
+                Integer.MAX_VALUE,
+                "The maximum amount of audio files that are cached in memory",
+                "Setting this to 0 will disable the cache",
+                "A higher value will result in less disk reads, but more RAM usage"
+        );
     }
 
 }
