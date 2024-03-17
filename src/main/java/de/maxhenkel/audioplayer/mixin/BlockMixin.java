50d5920d426838b9162fa1da33f4e545faa0aa5b
Max Henkel <max@maxhenkel.de>
2024-03-17 11:23:26 +0100

    Fix heads with custom audio don't keep data after placing down
    (cherry picked from commit 854b5547b681264376db353c3ec94a184b4dfdcc)

---
+++ src/main/java/de/maxhenkel/audioplayer/mixin/BlockMixin.java
@@ -0,0 +1,62 @@
+package de.maxhenkel.audioplayer.mixin;
+
+import de.maxhenkel.audioplayer.CustomSound;
+import de.maxhenkel.audioplayer.interfaces.CustomSoundHolder;
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SkullBlock;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.List;
+
+@Mixin(Block.class)
+public class BlockMixin {
+
+    @Inject(method = "getDrops(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/entity/BlockEntity;)Ljava/util/List;", at = @At(value = "RETURN"), cancellable = true)
+    private static void getDrops(BlockState blockState, ServerLevel serverLevel, BlockPos blockPos, @Nullable BlockEntity blockEntity, CallbackInfoReturnable<List<ItemStack>> ci) {
+        getDropsInternal(blockState, serverLevel, blockPos, blockEntity, ci);
+    }
+
+    @Inject(method = "getDrops(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/entity/BlockEntity;Lnet/minecraft/world/entity/Entity;Lnet/minecraft/world/item/ItemStack;)Ljava/util/List;", at = @At(value = "RETURN"), cancellable = true)
+    private static void getDrops(BlockState blockState, ServerLevel serverLevel, BlockPos blockPos, @Nullable BlockEntity blockEntity, @Nullable Entity entity, ItemStack itemStack, CallbackInfoReturnable<List<ItemStack>> ci) {
+        getDropsInternal(blockState, serverLevel, blockPos, blockEntity, ci);
+    }
+
+    @Unique
+    private static void getDropsInternal(BlockState blockState, ServerLevel serverLevel, BlockPos blockPos, @Nullable BlockEntity blockEntity, CallbackInfoReturnable<List<ItemStack>> ci) {
+        if (!(blockState.getBlock() instanceof SkullBlock)) {
+            return;
+        }
+        if (!(blockEntity instanceof CustomSoundHolder customSoundHolder)) {
+            return;
+        }
+        CustomSound customSound = customSoundHolder.soundplayer$getCustomSound();
+        if (customSound == null) {
+            return;
+        }
+
+        List<ItemStack> result = ci.getReturnValue();
+
+        for (ItemStack stack : result) {
+            if (!(stack.getItem() instanceof BlockItem blockItem)) {
+                continue;
+            }
+            if (!(blockItem.getBlock() instanceof SkullBlock)) {
+                continue;
+            }
+            customSound.saveToItem(stack);
+        }
+    }
+
+}
