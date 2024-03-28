3b19bb550b632f692cf9f77d078a3de20ac83657
Max Henkel <max@maxhenkel.de>
2024-03-28 15:19:42 +0100

    Implement upload endpoint and token authorization
    (cherry picked from commit f1f0d9867ce5599e5d2f922c58d54daf73f13acd)

--- src/main/java/de/maxhenkel/audioplayer/config/WebServerConfig.java
+++ src/main/java/de/maxhenkel/audioplayer/config/WebServerConfig.java
@@ -6,11 +6,13 @@ import de.maxhenkel.configbuilder.entry.ConfigEntry;
 public class WebServerConfig {
 
     public final ConfigEntry<Integer> port;
+    public final ConfigEntry<String> url;
+    //TODO Handle this in the webserver
+    public final ConfigEntry<String> basePath;
+    public final ConfigEntry<Long> tokenTimeout;
     //TODO
     // - Add basic auth
-    // - Add base path
     // - Configurable timeout
-    // - Own domain that's shown in-game
 
     public WebServerConfig(ConfigBuilder builder) {
         port = builder.integerEntry(
@@ -19,6 +21,25 @@ public class WebServerConfig {
                 1,
                 (int) Short.MAX_VALUE,
                 "The webserver port"
+        );
+        url = builder.stringEntry(
+                "url",
+                "",
+                "The URL under which the webserver is reachable",
+                "Example: https://test.example.com",
+                "If this is left empty, the user will be prompted to copy the token manually",
+                "If its set, the link will be generated automatically and the user can just open a link"
+        );
+        basePath = builder.stringEntry(
+                "base_path",
+                "",
+                "The base path under which the webserver is reachable",
+                "Example: /audioplayer"
+        );
+        tokenTimeout = builder.longEntry(
+                "token_timeout",
+                1000L * 60L * 5L,
+                "The timeout of the token in milliseconds"
         );
     }
 
