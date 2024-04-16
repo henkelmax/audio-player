8a06c5f300543b708bc5b699ef2da3f63270cda0
Max Henkel <max@maxhenkel.de>
2024-04-16 15:11:05 +0200

    Backport changes

--- src/main/java/de/maxhenkel/audioplayer/mixin/AbstractSkullBlockMixin.java
+++ src/main/java/de/maxhenkel/audioplayer/mixin/AbstractSkullBlockMixin.java
@@ -5,28 +5,32 @@ import de.maxhenkel.audioplayer.interfaces.ChannelHolder;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.AbstractSkullBlock;
+import net.minecraft.world.level.block.BaseEntityBlock;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.NoteBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.UUID;
 
 @Mixin(AbstractSkullBlock.class)
-public class AbstractSkullBlockMixin {
+public abstract class AbstractSkullBlockMixin extends BaseEntityBlock {
 
-    @Inject(method = "neighborChanged", at = @At(value = "HEAD"))
-    private void neighborChangedInject(BlockState blockState, Level level, BlockPos blockPos, Block block, BlockPos blockPos2, boolean bl, CallbackInfo ci) {
+    protected AbstractSkullBlockMixin(Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    public void neighborChanged(BlockState blockState, Level level, BlockPos blockPos, Block block, BlockPos blockPos2, boolean bl) {
         BlockState blockstate = level.getBlockState(blockPos.below());
         if (blockstate.getBlock() instanceof NoteBlock) {
+            super.neighborChanged(blockState, level, blockPos, block, blockPos2, bl);
             return;
         }
         BlockEntity blockEntity = level.getBlockEntity(blockPos);
         if (!(blockEntity instanceof ChannelHolder channelHolder)) {
+            super.neighborChanged(blockState, level, blockPos, block, blockPos2, bl);
             return;
         }
         UUID channelID = channelHolder.soundplayer$getChannelID();
@@ -34,6 +38,7 @@ public class AbstractSkullBlockMixin {
             PlayerManager.instance().stop(channelID);
         }
         channelHolder.soundplayer$setChannelID(null);
+        super.neighborChanged(blockState, level, blockPos, block, blockPos2, bl);
     }
 
 }
