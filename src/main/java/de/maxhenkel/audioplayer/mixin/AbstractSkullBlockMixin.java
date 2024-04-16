84e34a8644a5ab8215da302f48d01e69b88aa2d3
Max Henkel <max@maxhenkel.de>
2024-04-16 14:57:30 +0200

    Fix note blocks not stopping when moving them with a piston

---
+++ src/main/java/de/maxhenkel/audioplayer/mixin/AbstractSkullBlockMixin.java
@@ -0,0 +1,39 @@
+package de.maxhenkel.audioplayer.mixin;
+
+import de.maxhenkel.audioplayer.PlayerManager;
+import de.maxhenkel.audioplayer.interfaces.ChannelHolder;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.AbstractSkullBlock;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.NoteBlock;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.UUID;
+
+@Mixin(AbstractSkullBlock.class)
+public class AbstractSkullBlockMixin {
+
+    @Inject(method = "neighborChanged", at = @At(value = "HEAD"))
+    private void neighborChangedInject(BlockState blockState, Level level, BlockPos blockPos, Block block, BlockPos blockPos2, boolean bl, CallbackInfo ci) {
+        BlockState blockstate = level.getBlockState(blockPos.below());
+        if (blockstate.getBlock() instanceof NoteBlock) {
+            return;
+        }
+        BlockEntity blockEntity = level.getBlockEntity(blockPos);
+        if (!(blockEntity instanceof ChannelHolder channelHolder)) {
+            return;
+        }
+        UUID channelID = channelHolder.soundplayer$getChannelID();
+        if (channelID != null) {
+            PlayerManager.instance().stop(channelID);
+        }
+        channelHolder.soundplayer$setChannelID(null);
+    }
+
+}
