7b560b5ce40f842eebaf827e4b6d4d6fa5a7148e
Felix Dziatkowski <felix.dziatkowski@googlemail.com>
2024-04-18 19:17:56 +0200

    Add web UI for audio uploads

---
+++ web/vite.config.ts
@@ -0,0 +1,16 @@
+import { fileURLToPath, URL } from 'node:url';
+
+import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+});
