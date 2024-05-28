23b97a38a2136651af2bb9098da1415e3f862460
Max Henkel <max@maxhenkel.de>
2024-05-28 16:33:16 +0200

    Remove try-with-resources

--- src/main/java/de/maxhenkel/audioplayer/Filebin.java
+++ src/main/java/de/maxhenkel/audioplayer/Filebin.java
@@ -21,60 +21,60 @@ public class Filebin {
     public static void downloadSound(MinecraftServer server, UUID sound) throws IOException, InterruptedException, UnsupportedAudioFileException {
         String url = getBin(sound);
 
-        try (HttpClient client = HttpClient.newHttpClient()) {
-            HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create(url))
-                    .header("Accept", "application/json")
-                    .header("User-Agent", USER_AGENT)
-                    .build();
-            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .header("Accept", "application/json")
+                .header("User-Agent", USER_AGENT)
+                .build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
-            if (response.statusCode() != 200) {
-                throw new IOException("%s responded with status %s".formatted(url, response.statusCode()));
-            }
-
-            JsonElement json = JsonParser.parseString(response.body());
-
-            if (!(json instanceof JsonObject object)) {
-                throw new IOException("Invalid response");
-            }
-
-            JsonElement filesElement = object.get("files");
-
-            if (filesElement == null) {
-                throw new IOException("No files uploaded");
-            }
-
-            if (!(filesElement instanceof JsonArray files)) {
-                throw new IOException("No files uploaded");
-            }
-
-            for (JsonElement element : files) {
-                if (!(element instanceof JsonObject file)) {
-                    continue;
-                }
-
-                String contentType = file.get("content-type").getAsString();
-
-                if (contentType.equals("audio/wav") || contentType.equals("audio/mpeg")) {
-                    long size = file.get("bytes").getAsLong();
-
-                    if (size > AudioPlayer.SERVER_CONFIG.maxUploadSize.get()) {
-                        throw new IOException("Maximum file size exceeded (%sMB>%sMB)".formatted(Math.round((float) size / 1_000_000F), Math.round(AudioPlayer.SERVER_CONFIG.maxUploadSize.get().floatValue() / 1_000_000F)));
-                    }
-
-                    String filename = file.get("filename").getAsString();
-                    AudioManager.saveSound(server, sound, url + "/" + filename);
-                    deleteBin(url);
-                    return;
-                }
-            }
-            throw new IOException("No mp3 or wav files uploaded");
+        if (response.statusCode() != 200) {
+            throw new IOException("%s responded with status %s".formatted(url, response.statusCode()));
         }
+
+        JsonElement json = JsonParser.parseString(response.body());
+
+        if (!(json instanceof JsonObject object)) {
+            throw new IOException("Invalid response");
+        }
+
+        JsonElement filesElement = object.get("files");
+
+        if (filesElement == null) {
+            throw new IOException("No files uploaded");
+        }
+
+        if (!(filesElement instanceof JsonArray files)) {
+            throw new IOException("No files uploaded");
+        }
+
+        for (JsonElement element : files) {
+            if (!(element instanceof JsonObject file)) {
+                continue;
+            }
+
+            String contentType = file.get("content-type").getAsString();
+
+            if (contentType.equals("audio/wav") || contentType.equals("audio/mpeg")) {
+                long size = file.get("bytes").getAsLong();
+
+                if (size > AudioPlayer.SERVER_CONFIG.maxUploadSize.get()) {
+                    throw new IOException("Maximum file size exceeded (%sMB>%sMB)".formatted(Math.round((float) size / 1_000_000F), Math.round(AudioPlayer.SERVER_CONFIG.maxUploadSize.get().floatValue() / 1_000_000F)));
+                }
+
+                String filename = file.get("filename").getAsString();
+                AudioManager.saveSound(server, sound, url + "/" + filename);
+                deleteBin(url);
+                return;
+            }
+        }
+        throw new IOException("No mp3 or wav files uploaded");
     }
 
     public static void deleteBin(String url) {
-        try (HttpClient client = HttpClient.newHttpClient()) {
+        try {
+            HttpClient client = HttpClient.newHttpClient();
             HttpRequest request = HttpRequest.newBuilder()
                     .uri(URI.create(url))
                     .header("Accept", "application/json")
