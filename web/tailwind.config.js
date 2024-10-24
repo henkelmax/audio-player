d35d2ee36d906bee2f61ef117bbcb2f0d1827dbe
Max Henkel <max@maxhenkel.de>
2024-10-24 18:58:02 +0200

    Update web dependencies
    (cherry picked from commit e121566ccb1ad1915d32e20281d31fbbfc532a00)

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
