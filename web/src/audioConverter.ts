1ac4adbcb6a14996776b41eb8daaf41e060622c2
Max Henkel <max@maxhenkel.de>
2026-03-10 08:29:51 +0100

    Use mediabunny to convert audio
    (cherry picked from commit c718e17b57a60e3f8469fe05ccaee8a4377d4435)

---
+++ web/src/audioConverter.ts
@@ -0,0 +1,42 @@
+import {
+    Input,
+    Output,
+    Conversion,
+    ALL_FORMATS,
+    BlobSource,
+    Mp3OutputFormat,
+    canEncodeAudio, BufferTarget
+} from 'mediabunny';
+import {registerMp3Encoder} from "@mediabunny/mp3-encoder";
+
+export async function convertAudio(inputFile: File) {
+    if (!(await canEncodeAudio('mp3'))) {
+        registerMp3Encoder();
+    }
+
+    const input = new Input({
+        source: new BlobSource(inputFile),
+        formats: ALL_FORMATS,
+    });
+
+    const output = new Output({
+        format: new Mp3OutputFormat(),
+        target: new BufferTarget(),
+    });
+
+    const conversion = await Conversion.init({
+        input, output, audio: {
+            numberOfChannels: 1,
+        }
+    });
+
+    if (!conversion.isValid) {
+        throw new Error("Audio format not supported!")
+    }
+
+    await output.start();
+    await conversion.execute();
+    await output.finalize();
+
+    return new Blob([output.target.buffer!], { type: output.format.mimeType });
+}
